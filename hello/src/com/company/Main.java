4abbfa780fcc4ec3bd0cb0b34dc66730a7ecb2c0
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-04-20 12:56:39 +0900

    hello world

---
+++ hello/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.example.interpreter.InterpreterParser;
+import org.example.interpreter.InterpreterLexer;
+
+
+public class Main {
+    public static void main(String[] args) {
+        InterpreterLexer lexer = new InterpreterLexer(CharStreams.fromString("1+2+5"));
+        InterpreterParser parser = new InterpreterParser(new CommonTokenStream(lexer));
+
+        parser.start();
+        System.out.println("My parser has executed Order 66");
+    }
+}
